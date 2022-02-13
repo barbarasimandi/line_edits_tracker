4f24a05027e6fcb32f098b5a1c4e8cd0801b9697
Simándi Barbara <barbara.simandi@itron.com>
2022-02-13 13:21:57 +0100

    Add parsing to json, get pull requests one-by-one

--- tracker.rb
+++ tracker.rb
@@ -1,8 +1,9 @@
 require 'net/http'
+require 'json'
 
 class Tracker
   def self.get_response
-    url = URI("https://api.github.com/repos/rails/rails/pulls")
+    url = URI("https://api.github.com/repos/rails/rails/pulls?per_page=5")
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
@@ -10,7 +11,18 @@ class Tracker
     request = Net::HTTP::Get.new(url)
 
     response = https.request(request)
-    response.read_body
+
+    pull_requests = JSON.parse(response.read_body)
+
+    prs = []
+
+    pull_requests.each do |pull_request|
+      url = pull_request.fetch("url")
+      request = Net::HTTP::Get.new(url)
+
+      response = https.request(request)
+
+      prs << JSON.parse(response.read_body)
+    end
   end
 end
-
