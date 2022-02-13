1cb1d83640d8d64f4ae598b6ec8ec3707d5c55ab
Simándi Barbara <barbara.simandi@itron.com>
2022-02-13 14:03:14 +0100

    Symbolize keys of jsons, add bearer token

--- tracker.rb
+++ tracker.rb
@@ -1,16 +1,26 @@
 require 'net/http'
 require 'json'
 
+require 'byebug'
+
 class Tracker
+
+  BEARER_TOKEN = File.read('.bearer').chomp
+
   def self.get_pull_requests
     #TODO recursively read pages
     pull_requests = get_json("https://api.github.com/repos/rails/rails/pulls?per_page=15")
 
+    prs_with_multiple_commits = {}
+
     pull_requests.inject([]) do |prs, pull_request|
-      url = pull_request.fetch("url")
+      url = pull_request[:url]
       pr = get_json(url)
-      number_of_commits = pr.fetch("commits")
-      prs << pr if number_of_commits > 1
+      pr_number = pr[:number]
+
+      prs << pr if pr[:commits] > 1
+
+      prs_with_multiple_commits[pr_number] = prs
     end
   end
 
@@ -21,7 +31,8 @@ class Tracker
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
     request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = BEARER_TOKEN
     response = https.request(request)
-    JSON.parse(response.read_body)
+    JSON.parse(response.read_body, symbolize_names: true)
   end
 end
